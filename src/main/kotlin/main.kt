805537b534c4e34f85c9432ae650b3b1fe8846cc
Sungjin Mo <samuelmo78@gmail.com>
2019-11-08 14:14:44 -0600

    first commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,12 @@
+import java.util.Scanner
+
+fun main(arg: Array<String>) {
+
+    //Print an Integer from user input
+    val reader = Scanner(System.`in`)
+    print("Enter an Integer: ")
+1
+    var int: Int = reader.nextInt()
+    println("You entered $int")
+}
+
